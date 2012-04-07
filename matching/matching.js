d4e1aee7f4cd87ec33fa8c1497b042a7783e830d
Chris Lyons <lyons.chr@gmail.com>
2012-04-07 15:01:34 +0700

    first pass

---
+++ matching/matching.js
@@ -0,0 +1,107 @@
+jQuery(function() {
+  var MatchingGame;
+
+  MatchingGame = {
+
+    init: function() {
+      var cards;
+
+      cards = this.createPairs($('.pair'));
+
+      this.renderCards($('.container'), cards);
+    },
+
+    // pair up the items and return them all
+    createPairs: function($pairs) {
+      var $entry,
+          $firstCard,
+          $secondCard,
+          cards = [];
+
+      $pairs.each(function() {
+        var $entry,
+            $firstCard,
+            $secondCard,
+            $both;
+
+        $entry  = $(this);
+        $firstCard  = $entry.find('div:first');
+        $secondCard = $entry.find('div:nth-child(2)');
+        $both = $firstCard.add($secondCard);
+
+        // assign same id to both for later comparison
+        $both.data('id', cards.length);
+
+        cards.push($firstCard);
+        cards.push($secondCard);
+      });
+
+      return cards;
+    },
+
+    renderCards: function($container, cards) {
+      var _this = this;
+
+      // shuffle the array
+      cards.sort(function() {
+        return 0.5 - Math.random();
+      });
+
+      $.each(cards, function(i, cardContent) {
+        var $el;
+
+        $el = $('<div>').addClass('card face-down')
+                        .append(cardContent)
+                        .appendTo($container)
+                        .click( $.proxy(_this.cardClick, _this) )
+      });
+    },
+
+    // cardClick 'this' is bound to MatchingGame obj
+    cardClick: function(e) {
+      var $card, $faceUp;
+
+      $card = $(e.target);
+      $card.removeClass('face-down')
+           .addClass('face-up')
+
+      $faceUp = $('.face-up:not(.correct)');
+
+      if ($faceUp.length === 2) this.checkCorrect($faceUp);
+    },
+
+    checkCorrect: function($cards) {
+      var _this = this,
+          $firstCard,
+          $secondCard,
+          $bothCards,
+          $firstCardContent,
+          $secondCardContent;
+
+      $firstCard  = $($cards[0]);
+      $secondCard = $($cards[1]);
+      $bothCards  = $firstCard.add($secondCard);
+
+      $firstCardContent  = $firstCard.find('div:first');
+      $secondCardContent = $secondCard.find('div:first');
+
+      if ($firstCardContent.data('id') === $secondCardContent.data('id')) {
+        $bothCards.addClass('correct');
+        $('.card.face-down').click( $.proxy(_this.cardClick, _this) );
+      } else {
+        $('.card').unbind('click', this.cardClick);
+
+        $bothCards.addClass('incorrect');
+
+        $(document).one('mousedown', function(e) {
+          $bothCards.removeClass('face-up incorrect')
+                    .addClass('face-down');
+          $('.card.face-down').click( $.proxy(_this.cardClick, _this) );
+        });
+      }
+    }
+  };
+
+  MatchingGame.init();
+
+});
